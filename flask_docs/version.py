6401c45db4a5a80dd0bbf2973ed3bb308fd38e85
kwkw <wei497896143@yeah.net>
2021-10-08 15:35:34 +0000

    build: update version v0.5.3

--- flask_docs/version.py
+++ flask_docs/version.py
@@ -6,9 +6,9 @@ Program:
     Flask-Docs Version
 History:
     Created on 2021/06/06
-    Last modified on 2021/09/25
+    Last modified on 2021/10/08
 Author:
     kwkw
 """
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
